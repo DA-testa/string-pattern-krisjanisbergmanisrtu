eae8da0a34a2dc1b85ae1394da6d1e648a48bbfc
Krisjanis Bergmanis <Krisjanis.Bergmanis@edu.rtu.lv>
2023-04-04 22:09:56 +0300

    Set static path to file

--- hash_substring.py
+++ hash_substring.py
@@ -13,8 +13,8 @@ def read_input():
 
     if "F" in text:
         file_name = input()
-        file = open("./tests/" + file_name, "r")
-        # file = open("./tests/06", "r")
+        # file = open("./tests/" + file_name, "r")
+        file = open("./tests/06", "r")
         text = file.read()
         # after input type choice
         # read two lines
